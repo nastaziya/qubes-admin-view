008b4dba4e549299dfc77799f45b007af20a376b
CAnastasia <cotorobai.anastasia@gmail.com>
2019-11-06 08:45:39 +0100

    add components

---
+++ qubes-admin-vue/src/app/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="../assets/qubes-logo-icon.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Qubes Admin Panel'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+img{
+  max-width: 400px;
+  max-height: 200px
+}
+
+</style>
+
